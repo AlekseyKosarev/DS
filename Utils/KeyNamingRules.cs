205c4f6f8e87381f2266b904e79cc71ea49e24c8
AlekseyKosarev <57323671+AlekseyKosarev@users.noreply.github.com>
2025-08-20 12:30:46 +0500

    fix func - KeyFor

--- Utils/KeyNamingRules.cs
+++ Utils/KeyNamingRules.cs
@@ -9,11 +9,28 @@ namespace DS.Utils
         {
             var typeName = typeof(T).Name;
             var parts = new List<string> { typeName };
-
+        
             foreach (var arg in args)
+            {
                 if (arg != null)
-                    if(int.TryParse(arg.ToString(), out var argInt) && argInt >= 0)
+                {
+                    // Для чисел: добавляем все (включая отрицательные)
+                    if (arg is int intArg)
+                    {
+                        parts.Add(intArg.ToString());
+                    }
+                    // Для строк: добавляем все непустые
+                    else if (arg is string stringArg && !string.IsNullOrEmpty(stringArg))
+                    {
+                        parts.Add(stringArg);
+                    }
+                    // Для других типов: преобразуем в строку
+                    else
+                    {
                         parts.Add(arg.ToString());
+                    }
+                }
+            }
             return string.Join("_", parts).ToLowerInvariant();
         }
     }
